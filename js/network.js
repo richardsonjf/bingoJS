0fb0321f750536a37e9e46cd07d87c54e71e9e26
Jose Roa <jg.roac@gmail.com>
2015-03-27 15:06:53 -0430

    first commit

---
+++ js/network.js
@@ -0,0 +1,39 @@
+var dgram = require('dgram');
+var network = {
+    udp: dgram.createSocket('udp4'),
+    tcp: require('net'),
+
+    serverUDP : function(json,port){
+        var dgram = require('dgram');
+        var PORT = port;
+        var HOST = '255.255.255.255';
+        var server = dgram.createSocket('udp4');
+        var message = new Buffer(JSON.stringify(json));
+        server.bind(function(){
+            server.setBroadcast(true);
+        });
+
+        server.send(message,0,message.length,PORT,HOST,function (err,byte) {
+            if (err) throw err;
+            console.log('Message UDP sended to: ' + HOST + ' Port: ' + PORT);
+            server.close();
+        });
+    },
+
+    clientUDP: function (port){
+        var dgram = require('dgram');
+        var client = dgram.createSocket('udp4');
+        var PORT = port;
+        var HOST = '255.255.255.255';
+        client.on('listening',function(){
+            console.log("Server on listening:"+ HOST + ' Port:' + PORT);
+        });
+
+        client.on('message',function(message,remote){
+            console.log('message receive: ' + message);
+        });
+
+        client.bind(PORT,HOST);
+    }
+
+};
